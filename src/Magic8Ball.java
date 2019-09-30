0e0abbdb7cacffa3fd5c0d3f326b0f125657cd9c
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-30 09:36:07 -0400

    made the java class

---
+++ src/Magic8Ball.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args){
+
+        // declare
+        Scanner person;
+        int value;
+        int testing;
+        String question;
+
+        //inialization
+
+
+
+    }
+}
