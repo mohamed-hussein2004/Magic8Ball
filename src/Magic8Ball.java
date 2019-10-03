73417924650b1f5a0194e0201632ebed020154c1
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-10-03 09:16:47 -0400

    added the first 3 else if statements

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Magic8Ball {
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
         // declare
         Scanner person;
@@ -14,7 +14,23 @@ public class Magic8Ball {
         System.out.println("Think of a question then give me a number between 0-100");
         int firstNumber = person.nextInt();
 
-        if (10 >= firstNumber && firstNumber >0) {
+        if (10 >= firstNumber && firstNumber > 0) {
             System.out.print("yes");
 
         }
+        else if (20>= firstNumber && firstNumber>10){
+
+            System.out.print("you are correct");
+
+        }
+        else if (30>= firstNumber && firstNumber>20) {
+            System.out.print("no");
+
+        }
+        else if (40>= firstNumber && firstNumber>30) {
+            System.out.print("you  are ready");
+
+        }
+
+       
+    }
