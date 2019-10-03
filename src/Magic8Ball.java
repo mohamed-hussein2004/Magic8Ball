c2ad60823ec7086dfc6af63ec4443a29e4e9aa1f
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-10-03 09:15:26 -0400

    added my if statement

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -7,13 +7,14 @@ public class Magic8Ball {
         // declare
         Scanner person;
         person = new Scanner(System.in);
-
         int testing;
         String question;
 
         //inialization
+        System.out.println("Think of a question then give me a number between 0-100");
+        int firstNumber = person.nextInt();
 
+        if (10 >= firstNumber && firstNumber >0) {
+            System.out.print("yes");
 
-
-    }
-}
+        }
